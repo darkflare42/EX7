2968dd551a426581de1488baca9a9bf1f25c55d1
odedex <karitvruda@hotmail.com>
2014-06-10 07:56:16 +0300

    new class

---
+++ src/oop/ex7/FileReader.java
@@ -0,0 +1,41 @@
+package oop.ex7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Oded on 10/6/2014.
+ */
+public class FileReader {
+    Scanner scanner;
+
+    public FileReader (File file) throws FileNotFoundException{
+        scanner = new Scanner(file);
+    }
+
+    public boolean hasNext() {
+        return scanner.hasNext();
+    }
+
+    public boolean hasNext(Pattern pattern) {
+        return scanner.hasNext(pattern);
+    }
+
+    public boolean hasNext (String pattern) {
+        return scanner.hasNext(pattern);
+    }
+
+    public String next() {
+        return scanner.next();
+    }
+
+    public String next(Pattern pattern) {
+        return scanner.next(pattern);
+    }
+
+    public String next (String pattern) {
+        return scanner.next(pattern);
+    }
+}
