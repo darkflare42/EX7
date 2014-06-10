338355ef66c38ff96b565332e5113f7121dbbc06
odedex <karitvruda@hotmail.com>
2014-06-10 18:49:26 +0300

    new class

--- src/oop/ex7/Types.java
+++ src/oop/ex7/Types.java
@@ -4,9 +4,19 @@ package oop.ex7;
  * Created by Oded on 10/6/2014.
  */
 public enum Types {
-    INT,
-    STRING,
-    CHAR,
-    BOOLEAN,
-    DOUBLE
+    INT("int"),
+    STRING("String"),
+    CHAR("char"),
+    BOOLEAN("boolean"),
+    DOUBLE("double");
+
+    private final String nameString;
+
+    Types (String name) {
+        nameString = name;
+    }
+
+    public String toString() {
+        return nameString;
+    }
 }
