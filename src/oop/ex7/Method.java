a92acd1697f2b3997d52887e01ddb4bcbf3d87f4
odedex <karitvruda@hotmail.com>
2014-06-12 15:57:44 +0300

    new class

--- src/oop/ex7/Method.java
+++ src/oop/ex7/Method.java
@@ -8,24 +8,30 @@ import java.util.ArrayList;
 public class Method implements Expression {
     private VariableEnum type;
     private String name;
-    private ArrayList<Variable> variables;
+    private ArrayList<Expression> expressions;
 
 
     public Method (String returnType, String methodName, String args) throws VariableTypeException, MethodBadArgsException{
+        // May be redundant due to always needing reference to the expressions outside of this method's scope.
         if (returnType.equals("null")) {
             type = null;
         } else {
             type = VariableEnum.toEnum(returnType);
         }
         name = methodName;
-        variables = SetVariables(args);
+        expressions = SetVariables(args);
     }
 
-    private ArrayList<Variable> SetVariables(String args) throws VariableTypeException, MethodBadArgsException{
+    public Method (String returnType, String methodName, String args, ArrayList<Expression> outsideExpressions) throws VariableTypeException, MethodBadArgsException{
+        this(returnType, methodName, args);
+        expressions.addAll(outsideExpressions);
+    }
+
+    private ArrayList<Expression> SetVariables(String args) throws VariableTypeException, MethodBadArgsException{
         String[] arguments = args.split(",");
         String[] currentArgument;
         String argument;
-        ArrayList<Variable> newVariables= new ArrayList<Variable>();
+        ArrayList<Expression> newVariables= new ArrayList<Expression>();
         for (String arg: arguments) {
             argument = arg.trim();
             if (argument.matches(VariableEnum.Types()+"\\s+([a-zA-Z_]+)([\\w]*)")) {
@@ -39,8 +45,8 @@ public class Method implements Expression {
     }
 
     public void AddVariable (Variable variable) {
-        if (!variables.contains(variable)) {
-            variables.add(variable);
+        if (!expressions.contains(variable)) {
+            expressions.add(variable);
         }
     }
 
@@ -50,5 +56,9 @@ public class Method implements Expression {
 
     public VariableEnum getType () {
         return type;
+    }
+
+    public ArrayList<Expression> getExpressions() {
+        return expressions;
     }
 }
