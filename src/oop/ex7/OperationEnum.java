c6fe8cebb1ac00adc72d9afda6d4a7727e980b47
odedex <karitvruda@hotmail.com>
2014-06-12 21:23:37 +0300

    Made toEnum() dynamic

--- src/oop/ex7/OperationEnum.java
+++ src/oop/ex7/OperationEnum.java
@@ -20,16 +20,25 @@ public enum OperationEnum {
     }
 
     public static OperationEnum toEnum (String string) throws OperationTypeException{
-        if (string.equals("+")) {
-            return OperationEnum.ADD;
-        } else if (string.equals("-")) {
-            return OperationEnum.SUBTRACT;
-        } else if (string.equals("*")) {
-            return OperationEnum.MULTIPLY;
-        } else if (string.equals("/")) {
-            return OperationEnum.DIVIDE;
-        } else {
-            throw new OperationTypeException();
+        for (OperationEnum operation: OperationEnum.values()) {
+            if (string.equals(operation.toString())) {
+                return operation;
+            }
         }
+        throw new OperationTypeException();
     }
+
+//    public static OperationEnum toEnum (String string) throws OperationTypeException{
+//        if (string.equals("+")) {
+//            return OperationEnum.ADD;
+//        } else if (string.equals("-")) {
+//            return OperationEnum.SUBTRACT;
+//        } else if (string.equals("*")) {
+//            return OperationEnum.MULTIPLY;
+//        } else if (string.equals("/")) {
+//            return OperationEnum.DIVIDE;
+//        } else {
+//            throw new OperationTypeException();
+//        }
+//    }
 }
