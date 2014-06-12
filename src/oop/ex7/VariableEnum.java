a20113a7821c136e9addc5d75b33d5b0b811a67a
odedex <karitvruda@hotmail.com>
2014-06-12 21:22:23 +0300

    Made toEnum() dynamic

--- src/oop/ex7/VariableEnum.java
+++ src/oop/ex7/VariableEnum.java
@@ -23,20 +23,29 @@ public enum VariableEnum {
     }
 
     public static VariableEnum toEnum (String string) throws VariableTypeException {
-        if (string.equals("int")) {
-            return VariableEnum.INT;
-        } else if (string.equals("String")) {
-            return VariableEnum.STRING;
-        } else if (string.equals("char")) {
-            return VariableEnum.CHAR;
-        } else if (string.equals("boolean")) {
-            return VariableEnum.BOOLEAN;
-        } else if (string.equals("double")) {
-            return VariableEnum.DOUBLE;
-        } else {
-            throw new VariableTypeException();
+        for (VariableEnum type: VariableEnum.values()) {
+            if (string.equals(type.toString())) {
+                return type;
+            }
         }
+        throw new VariableTypeException();
     }
+
+//    public static VariableEnum toEnum (String string) throws VariableTypeException {
+//        if (string.equals("int")) {
+//            return VariableEnum.INT;
+//        } else if (string.equals("String")) {
+//            return VariableEnum.STRING;
+//        } else if (string.equals("char")) {
+//            return VariableEnum.CHAR;
+//        } else if (string.equals("boolean")) {
+//            return VariableEnum.BOOLEAN;
+//        } else if (string.equals("double")) {
+//            return VariableEnum.DOUBLE;
+//        } else {
+//            throw new VariableTypeException();
+//        }
+//    }
 
     /**
      * Dynamically create the regex expression for all the Variable types.
