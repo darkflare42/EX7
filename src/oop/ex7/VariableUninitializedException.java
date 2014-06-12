fbb8dbca3acb7e915f6f26e07ef51b02ace98e83
odedex <karitvruda@hotmail.com>
2014-06-12 21:29:38 +0300

    Added Assign overload to support Variable assignment

---
+++ src/oop/ex7/VariableUninitializedException.java
@@ -0,0 +1,10 @@
+package oop.ex7;
+
+/**
+ * Created by Oded on 12/06/14.
+ */
+public class VariableUninitializedException extends Exception{
+    public VariableUninitializedException() {
+        super();
+    }
+}
