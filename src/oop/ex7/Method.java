c6808a7cc46765d0f844eb6ead3afba3bc8826da
odedex <karitvruda@hotmail.com>
2014-06-15 13:06:24 +0300

    Added a .ValidateHeader method to class Method that recieves an array of VariableEnums and checks, in order, that they match the types of the Variables declared in the method header.

--- src/oop/ex7/Method.java
+++ src/oop/ex7/Method.java
@@ -8,7 +8,8 @@ import java.util.LinkedHashMap;
 public class Method implements Expression {
     private VariableEnum type;
     private String name;
-    private LinkedHashMap<String, Expression> expressions;
+    private LinkedHashMap<String, Expression> allExpressions;
+    private LinkedHashMap<String, Expression> headerExpressions;
 
 
     /**
@@ -20,7 +21,7 @@ public class Method implements Expression {
      * @throws MethodBadArgsException
      */
     public Method (String returnType, String methodName, String args) throws VariableTypeException, MethodBadArgsException{
-        // May be redundant due to always needing reference to the expressions outside of this method's scope.
+        // May be redundant due to always needing reference to the allExpressions outside of this method's scope.
         if (returnType.equals("void")) {
             type = null;
         } else {
@@ -28,12 +29,14 @@ public class Method implements Expression {
         }
         name = methodName.trim() + "()"; // Save a method name with parentheses to deal with variable and methods having
                                          // possibly the same name.
-        expressions = SetVariables(args);
+        headerExpressions = SetVariables(args);
+        allExpressions = new LinkedHashMap<String, Expression>(headerExpressions);
+
     }
 
     public Method (String returnType, String methodName, String args, LinkedHashMap<String,Expression> outsideExpressions) throws VariableTypeException, MethodBadArgsException{
         this(returnType, methodName, args);
-        expressions.putAll(outsideExpressions);
+        allExpressions.putAll(outsideExpressions);
     }
 
     private LinkedHashMap<String, Expression> SetVariables(String args) throws VariableTypeException, MethodBadArgsException{
@@ -54,9 +57,23 @@ public class Method implements Expression {
     }
 
     public void AddVariable (Variable variable) {
-        if (!expressions.containsValue(variable)) {
-            expressions.put(variable.getName(), variable);
+        if (!allExpressions.containsValue(variable)) {
+            allExpressions.put(variable.getName(), variable);
         }
+    }
+
+    public boolean ValidateHeader (VariableEnum[] hearderTypes) throws MethodBadArgsCountException, MethodTypeMismatchException{
+        if (headerExpressions.size() != hearderTypes.length) {
+            throw new MethodBadArgsCountException();
+        }
+        int i = 0;
+        for (Expression innerExpression : headerExpressions.values()) {
+            if (innerExpression.getType() != hearderTypes[i]) {
+                throw new MethodTypeMismatchException();
+            }
+            i++;
+        }
+        return true;
     }
 
     public String getName () {
@@ -71,7 +88,7 @@ public class Method implements Expression {
         return true;
     }
 
-    public LinkedHashMap<String, Expression> getExpressions() {
-        return expressions;
+    public LinkedHashMap<String, Expression> getAllExpressions() {
+        return allExpressions;
     }
 }
---
+++ src/oop/ex7/MethodTypeMismatchException.java
@@ -0,0 +1,10 @@
+package oop.ex7;
+
+/**
+ * Created by Oded on 15/6/2014.
+ */
+public class MethodTypeMismatchException extends Exception{
+    public MethodTypeMismatchException() {
+        super();
+    }
+}
