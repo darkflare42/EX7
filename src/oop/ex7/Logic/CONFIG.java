697b3e82e8c57c9c9a79c57f2492db1f2c52a86b
odedex <karitvruda@hotmail.com>
2014-06-17 16:57:49 +0300

    beautifcation

--- src/oop/ex7/Logic/CONFIG.java
+++ src/oop/ex7/Logic/CONFIG.java
@@ -20,6 +20,7 @@ public class CONFIG {
 
     public static final String variableReg = minusOrNot + VALID_NAME;
     public static final String methodReg = minusOrNot + "(([a-zA-Z_]+)([\\w]*)[(].*[)])"; //TODO wanted to make this more dynamic by adding the VALID_NAME string before the () suffix.
+//    public static final String methodReg = minusOrNot + VALID_NAME + "([(].*[)])";
     public static final String doubleReg = minusOrNot + "(\\d+\\.\\d+)";
     public static final String intReg = minusOrNot + "(\\d+)";
     public static final String variableArrayReg = minusOrNot + "(([a-zA-Z_]+)([\\w]*))\\s*(\\[" + START + intReg + OR + variableReg + END +"])";
